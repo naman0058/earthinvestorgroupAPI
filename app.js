c956ba837d9bbff69c8987f361ede72958f471cd
naman0058 <jnaman345@gmail.com>
2023-03-30 10:26:12 +0530

    a

--- app.js
+++ app.js
@@ -24,13 +24,18 @@ var cors = require('cors')
 
 var app = express();
 
+
+var corsOptions = {
+  origin: 'https://earthinvestorsgroup.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors())
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
